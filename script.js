3ef59b952ea52b3e0ea4a3765a001b7db0fa090f
Victor <adm.victorfernando@gmail.com>
2023-11-24 01:18:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,51 @@ $(document).ready(function() {
         $('#registerForm').show();
     });
 
-    $('#backBtn').click(function() {
+    $('#listUsersBtn').click(function() {
+        $('#buttons').hide();
+        $('#userList').show();
+    });
+
+    $('#backBtnLogin').click(function() {
+        $('#loginForm').hide();
+        $('#buttons').show();
+    });
+
+    $('#backBtnRegister').click(function() {
         $('#registerForm').hide();
         $('#buttons').show();
+    });
+
+    $('#backBtnUserList').click(function() {
+        $('#userList').hide();
+        $('#buttons').show();
+    });
+
+    $('#submitBtn').click(function() {
+        var email = $('#email').val();
+        var password = $('#password').val();
+        if(email === 'teste@email.com' && password === '1234') {
+            $('#loginForm').hide();
+            $('#dashboard').show();
+        }
+    });
+
+    $('#backBtnDashboard').click(function() {
+        $('#dashboard').hide();
+        $('#buttons').show();
+    });
+
+    $('#regSubmitBtn').click(function() {
+        var name = $('#name').val();
+        var email = $('#regEmail').val();
+        var password = $('#regPassword').val();
+        if(name && email && password) {
+            $('#registerForm').hide();
+            $('#successMessage').show();
+            setTimeout(function() {
+                $('#successMessage').hide();
+                $('#buttons').show();
+            }, 1000);
+        }
     });
 });
